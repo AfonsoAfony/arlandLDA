92bb5d00895fde07ac20dc5b1988d82d7d60b894
Afonso <Afonydasilva@gmail.com>
2026-02-21 01:01:25 +0100

    ajeitando os docs para deployment na vercel

--- ScrollRevealWrapper.jsx
+++ ScrollRevealWrapper.jsx
@@ -1,13 +1,15 @@
+"use client";
 import { useEffect } from "react";
 import ScrollReveal from "scrollreveal";
 
 export default function ScrollRevealWrapper({ children, className }) {
   useEffect(() => {
+    // initialize ScrollReveal only in the browser
     ScrollReveal().reveal(".reveal", {
-      duration: 2000,     // tempo da animação (ms)
-      origin: "bottom",   // direção de onde vem o conteúdo
-      distance: "50px",   // distância inicial
-      reset: false        // se true, anima toda vez que rolar
+      duration: 2000, // tempo da animação (ms)
+      origin: "bottom", // direção de onde vem o conteúdo
+      distance: "50px", // distância inicial
+      reset: false, // se true, anima toda vez que rolar
     });
   }, []);
 
